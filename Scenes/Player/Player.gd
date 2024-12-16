645f36b01e0c1c8e7c73b123305e254d1f400c53
ManuLazcano <manu.lazcano17@gmail.com>
2024-12-16 01:57:53 -0300

    refactor(player): add comments

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -5,17 +5,20 @@ const JUMP_SPEED : int = -1800
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
+	
 	if is_on_floor():
+		 # Si el juego no está en ejecución, mostrar la animación "idle"
 		if not get_parent().game_running:
 			$AnimatedSprite2D.play("idle")
 		else:
-			$RunCollision.disabled = false
+			$RunCollision.disabled = false # Habilitar la colisión para el estado de carrera
 			if Input.is_action_pressed("ui_accept"):
 				velocity.y = JUMP_SPEED				
 			elif Input.is_action_pressed("ui_down"):
-				$AnimatedSprite2D.play("duck")
-				$RunCollision.disabled = true
-			else:
+				$AnimatedSprite2D.play("down")
+				$RunCollision.disabled = true  # Deshabilitar colision de correr mientras está agachado
+			# Si no se presionan teclas de acción, reproducir la animación de correr
+			else: 
 				$AnimatedSprite2D.play("run")
 	else:
 		$AnimatedSprite2D.play("jump")
